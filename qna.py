7f59ef7105d42009c8368b38c2bcedd3ed17e3f9
Jigchu <111167590+Jigchu@users.noreply.github.com>
2023-09-28 18:55:15 +0800

    added a new file for general question

---
+++ qna.py
@@ -0,0 +1,35 @@
+def binary(prompt: str):
+	response = ""
+	result = None
+
+	while result == None:
+		response = input(prompt)
+		response = response.lower()
+		if response[0] == "y":
+			result = True
+		elif response[0] == "n":
+			result = False
+	
+	return result
+
+def mcq(prompt: str, items: list[str]):
+	item_num = len(items)
+	answer = item_num + 1
+	print(prompt)
+	print(__mcq_format(items))
+
+	while answer > item_num:
+		try:
+			answer = int(input(f"Choose from 1~{item_num}: "))
+		except ValueError:
+			print("Invalid input")
+	
+	return answer
+
+def __mcq_format(items: list[str]):
+	fstring = ""
+	
+	for index, item in enumerate(items):
+		fstring += f"{index + 1}: {item}    "
+	
+	return fstring
