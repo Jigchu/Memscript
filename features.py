54c94dd323e22f78ae96ba23832e771c30861280
Jigchu <111167590+Jigchu@users.noreply.github.com>
2023-09-30 18:17:09 +0800

    Moved all main functions into their own file

---
+++ features.py
@@ -0,0 +1,37 @@
+import os
+
+from memscript import *
+import qna
+import memorise as mem
+
+def clear_eval():
+	try:
+		eval_f = open("eval.txt", "a")
+	except FileNotFoundError:
+		return 0
+	eval_f.seek(0)
+	eval_f.truncate(0)
+	return 0
+
+def memorise():
+	cwd = os.getcwd()
+	memscripts = memscript("memscripts", "dir", cwd)
+	repeat = True
+	
+	script = mem._choose(memscripts)
+	
+	while repeat:
+		mem._test(script)
+		repeat = qna.binary("Do you want to repeat the script?\n")
+
+	return 0
+
+def view():
+	cwd = os.getcwd()
+	memscripts = memscript("memscripts", "dir", cwd)
+	repeat = True
+	
+	while repeat:
+		script = mem._choose(memscripts)
+		mem._display(script)
+		repeat = qna.binary("Would you like to look at another script?\n")
