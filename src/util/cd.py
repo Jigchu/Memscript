ba84297adb8ce09a1c14870b74e01cf8177492b6
Jigchu <111167590+Jigchu@users.noreply.github.com>
2023-10-15 20:06:53 +0800

    Made find be able to find files and directories faster

--- src/util/cd.py
+++ src/util/cd.py
@@ -11,21 +11,26 @@ class cd:
 	def __exit__(self, etype, value, traceback):
 		os.chdir(self.savedPath)
 
-def find_dir(search: str, root: str):
+def find(search: str, type: str, root: str):
+	dir = 1 if type == "dir" else 0 if type == "file" else -1
+	if dir == -1:
+		return ""
 	root_dir = __root(root)
 	walk_object = os.walk(root_dir)
+	path = ""
 
 	for r, dirs, files in walk_object:
-		for dir in dirs:
-			if dir == search:
-				return os.path.join(r, dir)
+		if type == "dir":
+			if search in dirs if dir == 1 else files:
+				path = os.path.join(r, search)
+	return path
 
 def __root(root: str):
 	cwd = os.getcwd()
 	prev_dir = cwd
 	curr_dir = None
 	
-	while curr_dir != root or curr_dir != "":
+	while curr_dir != root or curr_dir == "":
 		prev_dir, curr_dir = os.path.split(prev_dir)
 	
 	return os.path.join(prev_dir, curr_dir)
